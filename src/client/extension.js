7016c784bc49aa0cf394c1d6e97571f5032fb3b4
Marco <46133335+Marco4413@users.noreply.github.com>
2025-01-05 01:27:06 +0100

    Added LSP support.

---
+++ src/client/extension.js
@@ -0,0 +1,68 @@
+const vscode = require("vscode");
+
+const {
+    LanguageClient,
+    TransportKind
+} = require("vscode-languageclient/node");
+
+const EXTENSION_ID = "pulsarLanguage";
+
+/** @type {LanguageClient} */
+let g_Client;
+
+function GetLSPExecutablePath() {
+    const config = vscode.workspace.getConfiguration(EXTENSION_ID);
+    const execPath = config.get("lsp.path");
+    return execPath;
+}
+
+function StopLSP() {
+    if (g_Client) {
+        g_Client.stop();
+        g_Client = undefined;
+    }
+}
+
+function StartLSP() {
+    StopLSP();
+
+    const lspPath = GetLSPExecutablePath();
+    if (!lspPath) return;
+
+    /** @type {import("vscode-languageclient/node").ServerOptions} */
+    const serverOptions = {
+        run: { command: lspPath, transport: TransportKind.stdio },
+        debug: { command: lspPath, transport: TransportKind.stdio },
+    };
+
+    /** @type {import("vscode-languageclient").LanguageClientOptions} */
+    const clientOptions = {
+        documentSelector: [{ scheme: "file", language: "pulsar" }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher("**/*.pls")
+        }
+    };
+
+    g_Client = new LanguageClient(
+        "PulsarLSP",
+        "Pulsar LSP",
+        serverOptions,
+        clientOptions
+    );
+
+    g_Client.start();
+}
+
+/** @param {import("vscode").ExtensionContext} context */
+module.exports.activate = (context) => {
+    context.subscriptions.push(
+        vscode.commands.registerCommand(`${EXTENSION_ID}.lsp.start`, () => StartLSP()),
+        vscode.commands.registerCommand(`${EXTENSION_ID}.lsp.stop`,  () => StopLSP())
+    );
+
+    StartLSP();
+}
+
+module.exports.deactivate = () => {
+    StopLSP();
+}
